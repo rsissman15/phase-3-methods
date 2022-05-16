e3ff8330493985e92e01fd6592c5febc66c3ee23
Rebecca Sissman <rsissman15@aol.com>
2022-05-16 10:22:40 -0400

    commit tree e990c1ab6c0a41df8064abdcdf00c4d361563f35

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -52,3 +52,26 @@ describe '#halve' do
   end
 
 end
+
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+  puts "Hello, #{name}!"
+end
+
+def add(num1,num2)
+  num1 + num2
+end
+
+def halve(num1)
+  if num1.class != Integer
+    return nil
+  end
+  num1/2
+end
